e7ef12f99ee988f1da624ef0a9562047fcc7ea9c
Dawid <dawid@wierzycki.pl>
2024-01-13 21:04:43 +0100

    Update get endpoint by orders

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { ParseUUIDPipe, NotFoundException } from '@nestjs/common';
 
 @Controller('orders')
 export class OrdersController {
     constructor(private ordersService: OrdersService) {}
 
-    @Get('/')
+  @Get('/')
   getAll(): any {
     return this.ordersService.getAll();
   }
 
   @Get('/:id')
-  getById(@Param('id') id: string) {
-    return this.ordersService.getById(id);
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const order = await this.ordersService.getById(id);
+    if (!order) throw new NotFoundException('Order not found');
+    return order;
   }
 }
