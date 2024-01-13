8fff1ac825744276f48c6a73bd4229457d54db9a
Dawid <dawid@wierzycki.pl>
2024-01-13 18:01:04 +0100

    Add first get endpoint

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { db, Product } from './../db';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    public getAll(): Product[] {
+        return db.products;
+    }
+}
