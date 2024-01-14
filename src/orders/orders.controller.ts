07e3aa9049ffdd5f55a65209589bb7b08da30fb2
Dawid <dawid@wierzycki.pl>
2024-01-14 16:17:57 +0100

    Update orders endpoints and delete db.ts

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -14,17 +14,17 @@ export class OrdersController {
   }
 
   @Get('/:id')
-  getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    const order = this.ordersService.getById(id);
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const order = await this.ordersService.getById(id);
     if (!order) throw new NotFoundException('Order not found');
     return order;
   }
 
   @Delete('/:id')
-  deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
-    if (!this.ordersService.getById(id))
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.ordersService.getById(id)))
       throw new NotFoundException('Order not found');
-    this.ordersService.deleteById(id);
+    await this.ordersService.deleteById(id);
     return { success: true };
   }
 
@@ -34,14 +34,14 @@ export class OrdersController {
   }
 
   @Put('/:id')
-  update(
+  async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() orderData: UpdateOrderDTO,
   ) {
-    if (!this.ordersService.getById(id))
+    if (!(await this.ordersService.getById(id)))
       throw new NotFoundException('Order not found');
 
-    this.ordersService.updateById(id, orderData);
+    await this.ordersService.updateById(id, orderData);
     return { success: true };
   }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,33 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import { db, Order } from './../db';
 import { v4 as uuidv4 } from 'uuid';
+import { PrismaService } from '../shared/services/prisma.service';
+import { Order } from '@prisma/client'
 
 @Injectable()
 export class OrdersService {
-    public getAll(): Order[] {
-        return db.orders;
+    constructor(private prismaService: PrismaService) {}
+
+    public getAll(): Promise<Order[]> {
+        return this.prismaService.order.findMany();
     }
 
-    public getById(id: Order['id']): Order | null {
-        return db.orders.find((p) => p.id === id);
+    public getById(id: Order['id']): Promise<Order | null> {
+        return this.prismaService.order.findUnique({
+            where: { id },
+        });
     }
 
-    public deleteById(id: Order['id']): void {
-        db.orders = db.orders.filter((p) => p.id !== id);
+    public deleteById(id: Order['id']): Promise<Order> {
+        return this.prismaService.order.delete({
+        where: { id },
+        });
     }
 
-    public create(orderData: Omit<Order, 'id'>): Order {
-        const newOrder = { ...orderData, id: uuidv4() };
-        db.orders.push(newOrder);
-        return newOrder;
+    public create(
+        orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
+    ): Promise<Order> {
+        return this.prismaService.order.create({
+            data: orderData,
+        });
     }
       
-    public updateById(id: Order['id'], orderData: Omit<Order, 'id'>): void {
-        db.orders = db.orders.map((p) => {
-          if (p.id === id) {
-            return { ...p, ...orderData };
-          }
-          return p;
+    public updateById(
+        id: Order['id'],
+        orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
+    ): Promise<Order> {
+        return this.prismaService.order.update({
+            where: { id },
+            data: orderData,
         });
     }
 }
